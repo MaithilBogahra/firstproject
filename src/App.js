aff85e722913e557558f8769c10ffed26561ea1d
bogharamaithil3@gmail.com <bogharamaithil3@gmail.com>
2022-05-27 19:43:47 +0530

    initial setuo

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Demo from './component/Demo';
+
+function App() {
+  return (
+    <div>
+     <Demo />
+    </div>
+  );
+}
+
+export default App;
