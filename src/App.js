067246b4479385cb4c4db7ff1c9de6e12029b841
bogharamaithil3@gmail.com <bogharamaithil3@gmail.com>
2022-05-30 21:16:38 +0530

    array and object

--- src/App.js
+++ src/App.js
@@ -91,72 +91,143 @@ export default App;
 //   }
 // }
 
+// let data = {
+//     personal_info: {
+//       name: "maithil",
+//       age: 25,
+//       city: "surat"
+//     },
+//     course: ["C", "javaScript", "React"],
+//     branches: {
+//       rw1: {
+//         admission: 50,
+//         vacant_seat: 10
+//       },
+//       rw2: {
+//         admission: 30,
+//         vacant_seat: 20
+//       },
+//       rw3: {
+//         admission: 25,
+//         vacant_seat: 26
+//       },
+//       rw4: {
+//         admission: 40,
+//         vacant_seat: 10
+//       },
+//     }
+//   };
+
+// for (let i in data) {
+//   if (data[i] == data.personal_info) {
+//     for (let j in data.personal_info) {
+//       console.log(j, data.personal_info[j]);
+//     }
+//   } else if (data[i] == data.course) {
+//     // for (let k in data.course) {
+//     //   console.log(k,data.course[k]);
+//     // }
+//     data.course.map((p) => {
+//       console.log(p);
+//     })
+
+//   } else if (data[i] == data.branches) {
+//     for (let m in data.branches) {
+//       // console.log(data.branches[m]);
+//       if (data.branches[m] == data.branches.rw1) {
+//         for (let a in data.branches.rw1) {
+//           console.log(a, data.branches.rw1[a]);
+//         }
+//       }
+//       if (data.branches[m] == data.branches.rw2) {
+//         for (let b in data.branches.rw2) {
+//           console.log(b, data.branches.rw2[b]);
+//         }
+//       }
+//       if (data.branches[m] == data.branches.rw3) {
+//         for (let c in data.branches.rw3) {
+//           console.log(c, data.branches.rw3[c]);
+//         }
+//       }
+//       if (data.branches[m] == data.branches.rw4) {
+//         for (let d in data.branches.rw4) {
+//           console.log(d, data.branches.rw4[d]);
+//         }
+//       }
+
+//     }
+//   }
+// }
+
 let data = [
   {
-  personal_info: {
-    name: "maithil",
-    age: 25,
-    city: "surat"
-  },
-  course: ["C", "javaScript", "React"],
-  branches: {
-    rw1: {
-      admission: 50,
-      vacant_seat: 10
+    personal_info: {
+      name: "maithil",
+      age: 25,
+      city: "surat"
     },
-    rw2: {
-      admission: 30,
-      vacant_seat: 20
-    },
-    rw3: {
-      admission: 25,
-      vacant_seat: 26
-    },
-    rw4: {
-      admission: 40,
-      vacant_seat: 10
-    },
+    course: ["C", "javaScript", "React"],
+    branches: {
+      rw1: {
+        admission: 50,
+        vacant_seat: 10
+      },
+      rw2: {
+        admission: 30,
+        vacant_seat: 20
+      },
+      rw3: {
+        admission: 25,
+        vacant_seat: 26
+      },
+      rw4: {
+        admission: 40,
+        vacant_seat: 10
+      },
+    }
   }
-}
 ];
 
-for (let i in data) {
-  if (data[i] == data.personal_info) {
-    for (let j in data.personal_info) {
-      console.log(j, data.personal_info[j]);
-    }
-  } else if (data[i] == data.course) {
-    // for (let k in data.course) {
-    //   console.log(k,data.course[k]);
-    // }
-    data.course.map((p) => {
-      console.log(p);
-    })
 
-  } else if (data[i] == data.branches) {
-    for (let m in data.branches) {
-      // console.log(data.branches[m]);
-      if (data.branches[m] == data.branches.rw1) {
-        for (let a in data.branches.rw1) {
-          console.log(a, data.branches.rw1[a]);
-        }
+data.map((g) => {
+  for (let i in g) {
+    if (g[i] == g.personal_info) {
+      for (let j in g.personal_info) {
+        console.log(j, g.personal_info[j]);
       }
-      if (data.branches[m] == data.branches.rw2) {
-        for (let b in data.branches.rw2) {
-          console.log(b, data.branches.rw2[b]);
-        }
-      }
-      if (data.branches[m] == data.branches.rw3) {
-        for (let c in data.branches.rw3) {
-          console.log(c, data.branches.rw3[c]);
-        }
-      }
-      if (data.branches[m] == data.branches.rw4) {
-        for (let d in data.branches.rw4) {
-          console.log(d, data.branches.rw4[d]);
-        }
-      }
+    } else if (g[i] === g.course) {
+      // for (let k in g.course) {
+      //   console.log(k,g.course[k]);
+      // }
+     g.course.map((p) => {
+        console.log(p);
+      });
 
+    } else if (g[i] === g.branches) {
+      for (let m in g.branches) {
+        // console.log(data.branches[m]);
+        if (g.branches[m] === g.branches.rw1) {
+          for (let a in g.branches.rw1) {
+            console.log(a, g.branches.rw1[a]);
+          }
+        }
+        if (g.branches[m] === g.branches.rw2) {
+          for (let b in g.branches.rw2) {
+            console.log(b, g.branches.rw2[b]);
+          }
+        }
+        if (g.branches[m] === g.branches.rw3) {
+          for (let c in g.branches.rw3) {
+            console.log(c, g.branches.rw3[c]);
+          }
+        }
+        if (g.branches[m] === g.branches.rw4) {
+          for (let d in g.branches.rw4) {
+            console.log(d, g.branches.rw4[d]);
+          }
+        }
+
+      }
     }
   }
-}
+});
