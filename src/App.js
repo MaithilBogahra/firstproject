4724a6754c84978eac190dda8821cd0847dfbd4b
MaithilBoghara <bogharamaithil3@gmail.com>
2022-06-06 04:02:42 -0700

    Life-cycle-component

--- src/App.js
+++ src/App.js
@@ -4,12 +4,14 @@ import Countryclass from './Container/Countryclass';
 import Countryfun from './Container/Countryfun';
 import Cityclass from './Container/Cityclass';
 import Cityfun from './Container/Cityfun';
+import Time from './Container/Time/Time';
 
 function App() {
   return (
     <div>
+      <Time />
      {/* <Countryclass /> */}
-      <Countryfun />
+      {/* <Countryfun /> */}
      {/* <Cityclass /> */}
      {/* <Cityfun />  */}
     </div>
---
+++ src/Container/Time/Time.js
@@ -0,0 +1,40 @@
+import React, { Component } from 'react';
+
+class Time extends Component {
+    constructor(props) {
+        super(props);
+        
+        this.state ={
+            Time : new Date()
+        }
+    }
+
+tick =() =>{
+this.setState({
+    Time:new Date()
+})
+}
+
+    componentDidMount = () => {
+        this.TimeI = setInterval(() => this.tick(),1000)
+    }
+    
+    componentDidUpdate = (prevProps , prevState) =>{
+if(this.state.Time !== prevState.Time){
+    console.log("ComponentDidUpdate");
+}
+    }
+
+    componentWillUnmount = () => {
+        clearInterval(this.TimeI)
+    }
+    render() {
+        return (
+            <div>
+                <p>{this.state.Time.toLocaleTimeString()}</p>
+            </div>
+        );
+    }
+}
+
+export default Time;
